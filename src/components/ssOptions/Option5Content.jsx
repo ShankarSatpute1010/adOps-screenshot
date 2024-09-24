a6e16fcf7acc169d71194db92f0459d1f4c75c9a
Shankar Satpute <shankar@inuxu.media>
2024-09-24 13:11:06 +0530

    other options

--- src/components/ssOptions/Option5Content.jsx
+++ src/components/ssOptions/Option5Content.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import "./Option5.css";
 
-const Option5Content = () => {
+const Option5Content = ({
+  formattedDate,
+  siteFontSize,
+  logoCrop,
+  fontBrandText,
+  fontImageText,
+  brandText,
+  croppedImageUrl,
+  imageTop,
+  siteText,
+  siteTime,
+  imageText,
+  format,
+  startDate,
+  setImageTopSize,
+  imageTopSize
+}) => {
+  formattedDate = format(startDate, "HH:MM EEE, d MMM ");
+
+  useEffect(() => {
+    if(croppedImageUrl){
+      setImageTopSize(390);
+    }
+  }, [imageTopSize]);
+
   return (
-    <div>Option5Content</div>
-  )
-}
+    <div className="option-three">
+      <img
+        style={{ width: "100%" }}
+        src="https://demo.adgebra.in/custom/images/White_1.jpg"
+        alt="Blank Screenshot"
+      />
+      <span className="date-format-time">
+        {formattedDate ? formattedDate : ""}
+      </span>
+      <div className="heading-text-notif">
+        <span
+          className="image-text-site leftSize"
+          style={{ fontSize: siteFontSize }}
+        >
+          {siteText}
+        </span>
+        <span
+          className="leftSize"
+          style={{
+            position: "relative",
+            top: "-5px",
+            fontSize: "18px",
+          }}
+        >
+          .
+        </span>
+        <span
+          className="image-text-time leftSize"
+          style={{ fontSize: siteFontSize }}
+        >
+          {siteTime}
+        </span>
+      </div>
+      {logoCrop ? <img className="logo-crop" src={logoCrop} alt="Logo" /> : ""}
+      <span className="image-text-brand" style={{ fontSize: fontBrandText }}>
+        {brandText}
+      </span>
+      <span className="image-text-heading" style={{ fontSize: fontImageText }}>
+        {imageText}
+      </span>
+      <div
+        style={{ position: "absolute", width: "100%", height: "100%", top: 0 }}
+      >
+        <img
+          className="brand-image"
+          src={croppedImageUrl}
+          style={{
+            width: "280px",
+            height: "auto",
+            top: (imageTop?.value ? imageTop?.value : 355) + "px",
+          }}
+          alt="CropImage"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Option5Content;
